efc0a1664278d31713068827f3bb6d6659aad6f6
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-03-15 18:03:36 -0300

    projeto inicializado

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>Hello World! Baby!</h1>
+    </>
+  );
+}
