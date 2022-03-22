d4237e88a180b9b5f0441ba8fda9d4098a3bef6d
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-03-22 16:03:17 -0300

    criaçao e configuraçao do db fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,16 @@
-import NextAuth from "next-auth";
+import NextAuth, { Account, Profile, User } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
+
+interface signInProps {
+  user: User;
+  account: Account;
+  profile: Profile;
+  email: {
+    verificationRequest?: boolean;
+  };
+}
 
 export default NextAuth({
   providers: [
@@ -8,9 +19,33 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: 'read:user',
+          scope: "read:user",
         },
       },
     }),
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile }: signInProps) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
