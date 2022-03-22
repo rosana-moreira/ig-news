8d75aaa50493a320b58642d0ff440647c3a5d0e0
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-03-22 11:10:37 -0300

    criaçao do sistema de autenticaçao

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,17 +1,27 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
 
 export function SingInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.singInButton}>
+  const { data: session } = useSession();
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.singInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Rosana Moreira
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.singInButton}>
+    <button
+      type="button"
+      className={styles.singInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in whith Github
     </button>
