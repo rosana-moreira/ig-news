7d4552b4914dd1ef0c5dfc7b8a0c4480c2ed9b1d
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-05-14 16:04:00 -0300

    criação de testes unitarios dos pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,31 +4,33 @@ import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
 
-interface HomeProps {
+type HomeProps = {
   product: {
-    princeId: string;
-    amount: number;
+    priceId: string;
+    amount: string;
   };
-}
+};
 export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
+
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <span> 👏 Hey, welcome</span>
+          <span>👏 Hey, welcome</span>
           <h1>
             News about <br />
-            the <span>React </span>wold.
+            the <span>React</span> world.
           </h1>
           <p>
-            Get acess to all the publications <br />
+            Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.princeId} />
+          <SubscribeButton />
         </section>
+
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
@@ -45,10 +47,11 @@ export const getStaticProps: GetStaticProps = async () => {
       currency: "USD",
     }).format(price.unit_amount / 100),
   };
+
   return {
     props: {
       product,
     },
-    revalidate: 60 * 60 * 24, //24horas
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
